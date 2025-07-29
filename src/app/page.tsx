16fb5d022205b631d6d15a5af4313c080f961ec4
Info APSquared <info@apsquared.co>
2025-07-29 17:05:58 -0400

    add revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { getAllColleges } from "@/dbutil/collegedb";
 import { College } from "@/models/College";
 import DashboardClient from "@/mycomponents/DashboardClient";
 
+export const revalidate = 60;
+
 export default async function Dashboard() {
   // Load colleges data on the server
   const colleges = await getAllColleges() as College[];
